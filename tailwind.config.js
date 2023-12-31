2d3cbf7ac0485551289e08a4f64eb816c82f0023
minatisleeping <minatt2002@gmail.com>
2023-12-31 17:25:15 +0700

    Build coffee magazine

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        'xs': '480px',
+      },
       fontFamily: {
         Karla: ['Karla', 'sans-serif']
       },
